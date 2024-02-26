c438b678816ec1491e8929827a75adcc2f25fdbf
Vinzenz Mueller <vam8238@gmail.com>
2024-02-26 02:15:55 +0100

    numcapacitanceadded

--- capacitivesensing/numint.py
+++ capacitivesensing/numint.py
@@ -13,7 +13,6 @@ import matplotlib.pyplot as plt
 
 
 
-
 #Gauss_quadrature integrating f(R,r,z,x) with respect to x on [a,b]
 def gauss_quadrature(f, R, r, z, a, b):
     # Gauss two-point quadrature rule
@@ -62,7 +61,6 @@ def integrandEz(R, r, z, x):
     return numerator/denominator
 
 
-
 def Efield(R,r,z, a, b, N):
     Er = integrate(integrandEr, R, r, z, a, b, N)
     Ez = integrate(integrandEz, R, r, z, a, b, N)
@@ -73,19 +71,28 @@ def Efield(R,r,z, a, b, N):
 # Define integration limits
 a = 0
 b = 2*np.pi
+#number of subintervals
 N = 1000
-R1 = 0.7
-R2 = 3.2
-constant1 = 1
-constant2 = -1
+#radii of the two rings with R1<R2
+R1 = 7e-3#m
+R2 = 20.1e-3#m
+#charge and charge density
+q = 1
+qdens1 = q/(2*np.pi)
+qdens2 = -q/(2*np.pi)
+
+#constants
+epsilon = 8.854e-12 #farads per meter
+constant1 = qdens1/(4*np.pi*epsilon)
+constant2 = qdens2/(4*np.pi*epsilon)
 
 #z = 10
 #r = 40
 #print(integrate(integrandEy, R, r, z, a, b, N))
 
 # Define the grid
-r = np.linspace(0, 4, 20)
-z = np.linspace(-4, 4, 20)
+r = np.linspace(0, 0.04, 20)
+z = np.linspace(-0.04, 0.04, 20)
 rr, zz = np.meshgrid(r, z)
 
 # Define the vector field
@@ -99,9 +106,27 @@ U, V = vectorfield(rr, zz, a,b,N)
 
 # Plot the vector field
 plt.figure(figsize=(6, 6))
-plt.quiver(rr, zz, U, V, color='b', angles='xy', scale_units='xy', scale=10)
+plt.quiver(rr, zz, U, V, color='b', angles='xy', scale_units='xy', scale=50000000000000000)
 plt.xlabel('R')
 plt.ylabel('Z')
 plt.title('Electric field in the (r-z)-plane')
 plt.grid()
 plt.show()
+
+
+#compute voltage difference
+N1 = N
+N2 = N
+def voltage(R1, R2, a, b, N1, N2):
+    
+    x = np.linspace(R1, R2, N2+1)
+    dx = (R2-R1)/N2
+    E1 = constant1*Efield(R1,x,0, a, b, N1)
+    E2 = constant2*Efield(R2,x,0, a, b, N1)
+    Er = E1[0] + E2[0]
+    integral = np.sum(Er*dx)
+    return integral
+
+volts = voltage(R1, R2, a, b, N1, N2)
+print('capacitance: ',q/volts,'farads')
+
